34ec5a521842dbc8a0dec579a2bb2dd9ef37e266
peppeluc22 <gluca0260@gmail.com>
2023-05-17 15:46:54 +0200

    created rows with cards inside

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
-
+include __DIR__ . '/Models/Product.php';
 ?>
 
 
 <?php include './Components/header.php' ?>
 
 <main>
-    <?php include './Components/card.php' ?>
+    <div class="container my-5">
+        <div class="row bg-success p-2">
+            <h1 class="text-white">Oggetti per cane!</h1>
+
+            <?php foreach ($products as $product) { ?>
+                <?php if ($product->animal == 'dog') { ?>
+                    <?php include './Components/card.php' ?>
+                <?php } ?>
+            <?php } ?>
+
+        </div>
+
+        <div class="row bg-danger p-2">
+            <h1 class="text-white">Oggetti per gatto!</h1>
+
+            <?php foreach ($products as $product) { ?>
+                <?php if ($product->animal == 'cat') { ?>
+                    <?php include './Components/card.php' ?>
+                <?php } ?>
+            <?php } ?>
+
+        </div>
+    </div>
 </main>
 
 <?php include './Components/footer.php' ?>
