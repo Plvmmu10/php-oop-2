72a0a358aab041c638a73bc7f89383865a6d7c45
peppeluc22 <gluca0260@gmail.com>
2023-05-17 15:47:21 +0200

    created category

--- Models/Category.php
+++ Models/Category.php
@@ -1,11 +1,14 @@
 <?php
 
-class Category
+class Category extends Product
 {
     public $animal;
 
-    public function __construct(string $animal)
+
+    public function __construct(string $name, string $type, string $image, string $price, string $animal)
     {
+        parent::__construct($name, $type, $image, $price);
         $this->animal = $animal;
     }
+
 }
